005271bc531cdc298365f61f4216ba8488c4ea03
Suman Dhali <suman.d@alienity.com>
2024-08-16 13:23:27 +0530

    v2-social-media | Base url change

--- src/constant/loading/index.js
+++
@@ -1,4 +0,0 @@
-import { LinearGradient } from "expo-linear-gradient";
-import { createShimmerPlaceholder } from "react-native-shimmer-placeholder";
-
-export const ShimmerPlaceholder = createShimmerPlaceholder(LinearGradient);
