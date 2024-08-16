39fe558e2141274c30181f28336d395135377e1d
Suman Dhali <suman.d@alienity.com>
2024-08-16 13:14:54 +0530

    v2-social-media | Base url change

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		// Get rid of the CORS error
 		proxy: {
 			"/api": {
-				target: "http://localhost:4000",
+				target: "https://social-media-backend-server-rexl.onrender.com",
 				changeOrigin: true,
 				secure: false,
 			},
